ef44cb22a7abb20b86cf17c984fbdd9a93bac9d7
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-03-15 19:33:29 +0530

    form created

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ def index():
     car_models=sorted(car['name'].unique())
     year=sorted(car['year'].unique(), reverse=True)
     fuel_type=car['fuel_type'].unique()
-    return render_template('index.html', companies=companies, car_models=car_models, year=year, fuel_type=fuel_type)
+    return render_template('index.html', companies=companies, car_models=car_models, years=year, fuel_types=fuel_type)
 
 if __name__=="__main__":
     app.run(debug=True)
