494dd13a4bb95356a356fb83c70c700ee21e36d0
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-03-15 18:41:00 +0530

    first commit - index established

---
+++ application.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app=Flask(__name__)
+car=pd.read_csv("cleaned-car.csv")
+
+@app.route('/')
+def index():
+    companies=sorted(car['company'].unique())
+    car_models=sorted(car['name'].unique())
+    year=sorted(car['year'].unique(), reverse=True)
+    fuel_type=car['fuel_type'].unique()
+    return render_template('index.html', companies=companies, car_models=car_models, year=year, fuel_type=fuel_type)
+
+if __name__=="__main__":
+    app.run(debug=True)
