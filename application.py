ef2ff57115fa8d0ea3791b90828c9723c97e455f
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-03-15 20:17:28 +0530

    predict route created

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 
 app=Flask(__name__)
@@ -12,8 +12,16 @@ def index():
     fuel_type=car['fuel_type'].unique()
     return render_template('index.html', companies=companies, car_models=car_models, years=year, fuel_types=fuel_type)
 
-@app.route('predict', methods=['POST'])
-
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    company=request.form.get('company')
+    car_model=request.form.get('car_model')
+    year=int(request.form.get('year'))
+    fuel_type=request.form.get('fuel_type')
+    kms_driven=int(request.form.get('kilo_driven'))
+    print (company, year, fuel_type, kms_driven, car_model)
+    return ""
+    
 
 if __name__=="__main__":
     app.run(debug=True)
